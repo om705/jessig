efe49df61f17ab7da1cc592771fc7b832e3048ee
om705 <61368509+om705@users.noreply.github.com>
2024-02-25 11:43:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,21 @@
 import os
 from os import getenv
 
-API_ID = getenv("API_ID", "16478649")
+API_ID = getenv("API_ID", "26115463")
 
-API_HASH = getenv("API_HASH", "78dgjgsdgbsihgdia444yb85d9427894796c5c")
+API_HASH = getenv("API_HASH", "054557eca362bba15769130a5fadf0de")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "5844832724:AAGoKwY_Hnjtrgewjnalwixijy_7tHfBDA")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6956215581:AAF-vMdygW-I0jpf34amaAZYttJqzKO5QZc")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-sudo_group = -10021406843333
-sudo_user = 550468565
-log_channel = -100193547696574
+sudo_group = -1002022753943
+sudo_user = 6631088681
+log_channel = -1002033194166
 
 # try:
 #     ADMINS=[]
-#     for x in (os.environ.get("ADMINS", "5760012562 5044896938 6116624490 6230767081 5496342413 6347607291 6351476810").split()):
+#     for x in (os.environ.get("ADMINS", "6631088681 5044896938 6116624490 6230767081 5496342413 6347607291 6351476810").split()):
 #         ADMINS.append(int(x))
 # except ValueError:
 #         raise Exception("Your Admins list does not contain valid integers.")
